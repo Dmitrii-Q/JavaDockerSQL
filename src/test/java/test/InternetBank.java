a1b95a2a5b36cb29b4a64868e0dd0fe40865d982
Dmitrii <poltev.dmitrii.qa@gmail.com>
2024-01-06 13:10:59 +0300

    First commit

---
+++ src/test/java/test/InternetBank.java
@@ -0,0 +1,50 @@
+package test;
+
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.*;
+import page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanDatabase;
+
+public class InternetBank {
+    LoginPage loginPage;
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase(); }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exit login and password from sut test date")
+    void shouldSuccessfullyLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with in base and random verification code")
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно код! Попробуйте еще раз. ");
+    }
+
+}
